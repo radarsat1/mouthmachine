53f639086e53c73d67b64ccd890f4bea765f9b6e
Stephen Sinclair <sinclair@music.mcgill.ca>
2014-09-20 22:06:47 +0200

    Print clusters in a format that can be pasted into an xrns track.

--- dnb-pca.py
+++ dnb-pca.py
@@ -75,15 +75,65 @@ def do_kmeans(data, clusters=2, show=False):
             plot(d[0], d[1], 'o', alpha=0.5)
     return fit
 
-def plot_pieces_and_clusters(pieces, clusters):
+def plot_pieces_and_clusters(pieces, clusters, ignore=[]):
     n_clusters = max(clusters)+1
     colors = [cm.jet(float(x)/n_clusters) for x in range(n_clusters)]
     m = max([p.max() for p in pieces])
     n = min([p.min() for p in pieces])
+    artists = []
     for i in xrange(len(pieces)):
         plot(arange(width*i, width*(i+1)), pieces[i], 'k-', alpha=0.5)
-        fill_between([width*i, width*(i+1)], [m]*2, [n]*2,
-                     color = colors[clusters[i]], alpha = 0.5)
+        if not clusters[i] in ignore:
+            a = fill_between([width*i, width*(i+1)], [m]*2, [n]*2,
+                             color = colors[clusters[i]], alpha = 0.5,
+                             picker = 1)
+            artists.append(a)
+    return artists
+
+# User must click on which clusters to *ignore*
+def plot_pieces_and_clusters_interactive(pieces, clusters):
+    fig, ax = subplots(1,1)
+    artists = plot_pieces_and_clusters(pieces, clusters)
+    ignore = []
+    def make_on_pick(artists):
+        def on_pick(event):
+            i = 0
+            try:
+                i = artists.index(event.artist)
+            except:
+                return True
+            ignore.append(clusters[i])
+            print 'ignore',set(ignore)
+            return True
+        return on_pick
+    fig.canvas.mpl_connect('pick_event', make_on_pick(artists))
+    show()
+    print 'ignore',set(ignore)
+    plot_pieces_and_clusters(pieces, clusters, ignore)
+    show()
+    return array([(-1 if c in ignore else c) for c in clusters])
+
+# print out the beats in an xrns-similar format that can be pasted
+# into a Song.xml file
+def print_xrns(beats):
+    k = 0
+    for i in range(0,max(beats)+1):
+        last_b = -1
+        one = False
+        for n,b in enumerate(beats):
+            if b == i and b != last_b and n < 128:
+                print """            <Line index="%d">
+              <NoteColumns>
+                <NoteColumn>
+                  <Note>C-4</Note>
+                  <Instrument>%02d</Instrument>
+                </NoteColumn>
+              </NoteColumns>
+            </Line>"""%(n, k)
+                one = True
+            last_b = b
+        if one:
+            k += 1
 
 pieces, features = make_pieces(width, mouth)
 #figure(1)
@@ -92,6 +142,6 @@ pieces, features = make_pieces(width, mouth)
 #clusters = do_kmeans(pca_features(features), clusters=6)
 clusters = do_kmeans(features, clusters=6)
 
-plot_pieces_and_clusters(pieces, clusters)
+beats = plot_pieces_and_clusters_interactive(pieces, clusters)
 
-show()
+print_xrns(beats)
